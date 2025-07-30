7f7e6f7e58e4d259d893d63f24e88640e376a4c1
khannaveed2310 <88425779+khannaveed2310@users.noreply.github.com>
2025-07-31 00:08:00 +0530

    add new app (#4)
    * New home app added
    
    * Update views.py

--- pizzafy_project/core/urls.py
+++ pizzafy_project/core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    # path('', views.index, name="index"),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.CustomTokenObtainPairView.as_view(), name='login'),  
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
